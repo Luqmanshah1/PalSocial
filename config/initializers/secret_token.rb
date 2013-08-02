b37fde59c37d5b07427c09a340b688d3af23f878
Luqman Shah <luqmanshah390@yahoo.com>
2013-08-02 03:16:45 -0400

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PalSocial::Application.config.secret_token = '731ef1cd788d9992810ad92a94093ba457bdd01420422601650e99d1bf7cbf9b770180b673bd8d66ba6f6c1577d2cdcc36d5c2c56b2d1b31082b2f10c76d6608'
